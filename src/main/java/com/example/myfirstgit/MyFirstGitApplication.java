f4706c946fb61319faa65d9b803e4144e7683cac
Joseph Sun <joseph@JosephdeMacBook-Air.local>
2023-03-25 07:52:01 +0800

    first commit

---
+++ src/main/java/com/example/myfirstgit/MyFirstGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfirstgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myfirstgit/MyFirstGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfirstgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
